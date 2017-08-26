8f8cd4f16e10ecbf2f2f264f1abe68869f8d2732
Mayck Xavier <mayckxavier@gmail.com>
2017-08-25 23:02:02 -0300

    Adição de campos de usuário

--- cruds-api/src/models/Usuario.php
+++ cruds-api/src/models/Usuario.php
@@ -3,5 +3,14 @@
 class Usuario extends \Illuminate\Database\Eloquent\Model
 {
     protected $table = 'usuarios';
-    protected $fillable = array('first_name', 'last_name', 'facebook_id');
+    protected $fillable = array(
+        'first_name',
+        'last_name',
+        'facebook_id',
+        'email',
+        'facebook_id',
+        'facebook_access_token',
+        'payment_transaction_id',
+        'access_expiration_date',
+        'welcome_email_sent');
 }
