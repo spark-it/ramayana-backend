33d87bc6f53f827c3b1742be9db0406ac74b74f0
Mayck Xavier <mayckxavier@gmail.com>
2017-08-30 05:13:05 -0300

    Adicionando atributo thumb no model Video

--- cruds-api/src/models/Video.php
+++ cruds-api/src/models/Video.php
@@ -3,5 +3,5 @@
 class video extends \Illuminate\Database\Eloquent\Model
 {
     protected $table = 'videos';
-    protected $fillable = array('title', 'description', 'text', 'category','video_link');
+    protected $fillable = array('title', 'description', 'text', 'category', 'video_link', 'thumb');
 }
